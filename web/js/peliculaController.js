a8cb14351382cbcef974d289d4656117b915048a
Yeison Lucio <yeisonlucio1266@gmail.com>
2018-11-08 07:01:07 -0500

    load vista pelicula

--- web/js/peliculaController.js
+++ web/js/peliculaController.js
@@ -1,12 +1,12 @@
 $(function(){
     $('.sidenav').sidenav();
-    $('#li_login').click(function(){
-       console.log("voy a cargar la vista login");
-      $("#contenedor").load('/login');
+    $('.li_login').click(function(){
+      //console.log("voy a cargar la vista login");
+     $("#contenedor").load('/login #contenedor_login');
     });
-    $('#li_peliculas').click(function(){
+    $('.li_peliculas').click(function(){
       console.log("voy a cargar la vista login");
-     $("#contenedor").load('/');
+     $("#contenedor").load('/ #contenedor_pelicula');
    });
   
   });
